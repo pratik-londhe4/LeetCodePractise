cd75534ecad0881eaf86e571612657fe6d9dd20f
Pratik Londhe <84666310+pratik-londhe4@users.noreply.github.com>
2022-11-22 21:43:22 +0530

    Time: 17 ms (9.08%), Space: 54.7 MB (17.75%) - LeetHub

--- 143-reorder-list/143-reorder-list.java
+++ 143-reorder-list/143-reorder-list.java
@@ -10,58 +10,20 @@
  */
 class Solution {
     public void reorderList(ListNode head) {
-        
-        //find mid of the list
         ListNode mid = middleNode(head);
-        
-        //break a new linked list from mid
-        ListNode head2 = mid.next;
+       ListNode head2 = mid.next;
         mid.next = null;
         
-        //reverse the half linked list
-       head2 = reverseList(head2);
+        head2 = reverseList(head2);
         
-        //merge both linked lists
-        
-        head = merge(head,head2);
-        
-        
-    }
-    
-    public ListNode merge(ListNode head1 ,ListNode head2){
-        ListNode dummy = new ListNode(-1);
-        ListNode p1 = head1;
-        ListNode p2 = head2;
-        ListNode curr = dummy;
-        
-        while(p1 != null && p2 != null){
-            ListNode nextofP1 = p1.next;
-            p1.next = null;
-            
-            ListNode nextofP2 = p2.next;
-            p2.next = null;
-            
-            //merge p1
-            curr.next = p1;
-            p1 = nextofP1;
-            curr = curr.next;
-            
-            //merge p2
-            curr.next = p2;
-            p2 = nextofP2;
-            curr = curr.next;
-        }
-        
-        curr.next = p1;
-        
-        return dummy.next;
+       head =  merge(head,head2);
     }
     
     public ListNode middleNode(ListNode head){
         ListNode slow = head;
         ListNode fast = head;
         
-        while(fast.next!=null && fast.next.next != null){
+        while(fast.next != null && fast.next.next != null){
             slow = slow.next;
             fast = fast.next.next;
         }
@@ -74,14 +36,42 @@ class Solution {
         ListNode curr = head;
         
         while(curr != null){
-        ListNode nextOfCurr = curr.next;
-
+            ListNode nextofCurr = curr.next;
+            
             curr.next = prev;
             prev = curr;
-            curr = nextOfCurr;
+            curr = nextofCurr;
+            
         }
         
         return prev;
+    }
+    
+    public ListNode merge(ListNode head1 , ListNode head2){
+        ListNode dummy = new ListNode(-1);
+        ListNode p1 = head1;
+        ListNode p2 = head2;
+        ListNode curr = dummy;
+        
+        while(p1 != null && p2 != null){
+            ListNode nextofP1 = p1.next;
+            p1.next = null;
+            
+            ListNode nextofP2 = p2.next;
+            p2.next = null;
+            
+            curr.next = p1;
+            p1 = nextofP1;
+            curr = curr.next;
+            
+            curr.next = p2;
+            p2 = nextofP2;
+            curr = curr.next;
+        }
+        
+        curr.next = p1;
+        
+        return dummy.next;
         
     }
 }
