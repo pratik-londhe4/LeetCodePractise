db6c6344e216cf7b378bbc0f5bdb7c81bf1c59eb
Pratik Londhe <84666310+pratik-londhe4@users.noreply.github.com>
2022-12-21 23:28:02 +0530

    Time: 33 ms (22.14%), Space: 70.9 MB (24.25%) - LeetHub

---
+++ 217-contains-duplicate/217-contains-duplicate.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        HashMap<Integer,Integer> hm = new HashMap();
+        
+        for(int i : nums){
+            hm.put(i , hm.getOrDefault(i,0) + 1);
+            if(hm.get(i) > 1)return true;
+        }      
+        
+        return false;
+    }
+}
