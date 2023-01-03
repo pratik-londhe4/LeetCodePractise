2a979969d578cbb062c411ece751e32f44daecb5
Pratik Londhe <84666310+pratik-londhe4@users.noreply.github.com>
2023-01-03 09:17:46 +0530

    Time: 17 ms (44.72%), Space: 47.4 MB (58.20%) - LeetHub

--- 944-delete-columns-to-make-sorted/944-delete-columns-to-make-sorted.java
+++ 944-delete-columns-to-make-sorted/944-delete-columns-to-make-sorted.java
@@ -3,8 +3,8 @@ class Solution {
         int ans = 0;
         
         for(int i = 0 ; i < strs[0].length() ; i++){
-            for(int j = 1; j < strs.length ; j++){
-                if(strs[j].charAt(i) < strs[j-1].charAt(i)){
+            for(int j = 0; j < strs.length-1 ; j++){
+                if(strs[j].charAt(i) > strs[j+1].charAt(i)){
                     ans++;
                     break;
                 }
