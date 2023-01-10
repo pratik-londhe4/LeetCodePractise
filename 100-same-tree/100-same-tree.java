aab4ebf4b10eb6d425fff0acb71a34b9ae6f23e9
Pratik Londhe <84666310+pratik-londhe4@users.noreply.github.com>
2023-01-11 01:32:58 +0530

    Time: 0 ms (100.00%), Space: 39.7 MB (80.94%) - LeetHub

--- 100-same-tree/100-same-tree.java
+++ 100-same-tree/100-same-tree.java
@@ -15,58 +15,12 @@
  */
 class Solution {
     public boolean isSameTree(TreeNode p, TreeNode q) {
-      String in1 = inorderPrint(p);
-        String in2 = inorderPrint(q);
-        System.out.println(in1);
-        System.out.println(in2);
-        return in1.equals(in2) && preorderPrint(p).equals(preorderPrint(q));
+      if(p == null && q == null)return true;
+    if(p == null || q == null)return false;
+    
+        return p.val == q.val &&  isSameTree(p.left,q.left) && isSameTree(p.right,q.right);
     }
     
     
-    
-    String inorderPrint(TreeNode p){    
-    
-    ArrayList<Integer> arr = new ArrayList();
-        inorder(p,arr);
-        String ans = "";
-        for(int i = 0 ; i < arr.size(); i++){
-            ans+=arr.get(i);
-        }
-        
-        return ans;
-        
-    }
-    
-    void inorder(TreeNode p, ArrayList<Integer> arr){
-        if(p == null){
-            arr.add(-1);
-            return;
-        }
-        inorder(p.left,arr);
-        arr.add(p.val);
-        inorder(p.right,arr);
-    }
-    
-    
-    String preorderPrint(TreeNode p){
-         ArrayList<Integer> arr = new ArrayList();
-        preorder(p,arr);
-        String ans = "";
-        for(int i = 0 ; i < arr.size(); i++){
-            ans+=arr.get(i);
-        }
-        
-        return ans;
-        
-    }
-    
-    void preorder(TreeNode p, ArrayList<Integer> arr){
-        if(p == null){
-            arr.add(-1);
-            return;
-        }
-        arr.add(p.val);
-        preorder(p.left,arr);
-        preorder(p.right,arr);
-    }
+   
 }
