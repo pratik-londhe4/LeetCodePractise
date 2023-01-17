04289e675f4ad696220e89ab94e91bd413172713
Pratik Londhe <84666310+pratik-londhe4@users.noreply.github.com>
2023-01-17 21:18:49 +0530

    Time: 0 ms (100.00%), Space: 41.8 MB (84.26%) - LeetHub

--- 35-search-insert-position/35-search-insert-position.java
+++ 35-search-insert-position/35-search-insert-position.java
@@ -1,26 +1,23 @@
 class Solution {
     public int searchInsert(int[] nums, int target) {
-        //always check for the boundry cases
-        if(target < nums[0]){
-            return 0;
-        }
-        if(target > nums[nums.length-1]){
-            return nums.length;
-        }
-        int left = 0;
-        int right = nums.length-1;
-        while(left <= right){
-            int mid = (left+right)/2;
+        int n = nums.length;
+        if(target > nums[n-1])return n;
+        int potential = -1;
+        
+        int low = 0;
+        int high = nums.length-1;
+        
+        while(low <= high){
+            int mid = (low+high)/2;
             if(nums[mid] == target){
                 return mid;
-            }
-            
-            if(nums[mid] < target){
-                left = mid+1;
+            }else if(nums[mid] > target){
+                potential = mid;
+                high = mid-1;
             }else{
-                right = mid-1;
+                low = mid+1;
             }
         }
-        return left;
+        return potential;
     }
 }
