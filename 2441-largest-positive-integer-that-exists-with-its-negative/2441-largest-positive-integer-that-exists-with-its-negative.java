514d7f8f9a5e9ddb95771dde4575c26646ceb1c1
Pratik Londhe <84666310+pratik-londhe4@users.noreply.github.com>
2024-05-03 01:04:22 +0530

    Time: 2 ms (96.14%), Space: 45.2 MB (13.43%) - LeetHub

--- 2441-largest-positive-integer-that-exists-with-its-negative/2441-largest-positive-integer-that-exists-with-its-negative.java
+++ 2441-largest-positive-integer-that-exists-with-its-negative/2441-largest-positive-integer-that-exists-with-its-negative.java
@@ -3,28 +3,22 @@ class Solution {
         
         int[] map = new int[1001];
         
-        // map for all negative numbers
-         for(int i = 0 ; i < nums.length ; i++){
+        // add the -ve elements to the map
+        for(int i  = 0 ; i < nums.length ; i++){
             if(nums[i] < 0){
-                 map[Math.abs(nums[i])] = nums[i];
+                map[Math.abs(nums[i])] = nums[i];
             }
-            
-           
-            
         }
         
         int max = -1;
         
         for(int i = 0 ; i < nums.length ; i++){
-            if(nums[i] > 0 && map[nums[i]] == -nums[i]){
+            
+            if(nums[i] > 0  && map[nums[i]] == -nums[i]){
                 max = Math.max(max, nums[i]);
-                
             }
-            
-           
-            
         }
-        
+    
         return max;
     }
 }
