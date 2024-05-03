6382f9afaa2e24f82135fbb765f337684cc6de87
Pratik Londhe <84666310+pratik-londhe4@users.noreply.github.com>
2024-05-03 21:54:36 +0530

    Time: 1 ms (78.87%), Space: 41.9 MB (5.03%) - LeetHub

--- 0165-compare-version-numbers/0165-compare-version-numbers.java
+++ 0165-compare-version-numbers/0165-compare-version-numbers.java
@@ -1,39 +1,21 @@
 class Solution {
     public int compareVersion(String version1, String version2) {
-        // lets split the string first
+        // SPlit the string into array
         String[] v1 = version1.split("\\.");
         String[] v2 = version2.split("\\.");
         
-        int size = Math.max(v1.length, v2.length);
-        
-        // fill the small array with 0's
-        
-        if(v1.length < size){
+        for(int i = 0 ; i < Math.max(v1.length , v2.length) ; i++ ){
+            int num1 = i < v1.length ? Integer.parseInt(v1[i]) : 0;
+            int num2 = i < v2.length ? Integer.parseInt(v2[i]) : 0;
             
-            int length = v1.length;
-            v1 = new String[size];
-            Arrays.fill(v1, "0");
-            System.arraycopy(version1.split("\\."), 0, v1, 0, length);
-        }else if(v2.length < size){
-            
-            int length = v2.length;
-            v2 = new String[size];
-            Arrays.fill(v2, "0");
-            System.arraycopy(version2.split("\\."), 0, v2, 0, length);
-        }
-        
-        System.out.println(v2[0]);
-        
-        
-        for(int i = 0 ; i < size ; i++){
-            if(Integer.parseInt(v1[i]) > Integer.parseInt(v2[i])){
-                return 1;
-            }else if(Integer.parseInt(v1[i]) < Integer.parseInt(v2[i])){
+            if(num1 < num2){
                 return -1;
+                
+            }else if(num1 > num2){
+                return 1;
             }
         }
-        
+
         return 0;
-        
     }
 }
