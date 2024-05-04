b624a222eb0abe5d56d6d9184b0ff6a20b6c5641
Pratik Londhe <84666310+pratik-londhe4@users.noreply.github.com>
2024-05-04 21:19:34 +0530

    Time: 16 ms (97.17%), Space: 55.2 MB (11.75%) - LeetHub

--- 0881-boats-to-save-people/0881-boats-to-save-people.java
+++ 0881-boats-to-save-people/0881-boats-to-save-people.java
@@ -1,21 +1,26 @@
 class Solution {
     public int numRescueBoats(int[] people, int limit) {
-        Arrays.sort(people);
+      // sort the array
+        Arrays.sort(people); // nlongn
         
-        int left= 0 , right = people.length-1;
+        int left = 0;
+        int right = people.length -1;
+        
         int boats = 0;
         
         while(left <= right){
-            // add the heviest person first
-            int remaining = limit - people[right];
+            // add the heaviest person to the boat;
             boats++;
+            int remain = limit - people[right];
             right--;
             
-            // check if one more perosn can be added
-            if(people[left] <= remaining){
+            // second person can be added
+            if(left <= right && people[left] <= remain){
                 left++;
             }
         }
+        
         return boats;
+              
     }
 }
