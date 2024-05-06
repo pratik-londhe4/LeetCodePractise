ce22a0e82966a3f3dd2c19bcced4d3724a838041
Pratik Londhe <84666310+pratik-londhe4@users.noreply.github.com>
2024-05-07 00:05:08 +0530

    Time: 17 ms (40.84%), Space: 62.1 MB (86.14%) - LeetHub

--- 2487-remove-nodes-from-linked-list/2487-remove-nodes-from-linked-list.java
+++ 2487-remove-nodes-from-linked-list/2487-remove-nodes-from-linked-list.java
@@ -10,63 +10,48 @@
  */
 class Solution {
     public ListNode removeNodes(ListNode head) {
-        ListNode reversed = reverseList(head);
+       
+        // Reverse the list
+        ListNode reverse = reverseList(head);
+        
         Stack<Integer> stack = new Stack<>();
-        int max = 0;
-        ListNode reversedHead = reversed;
-        ListNode prev = null;
-        while(reversed != null){
-            if(max > reversed.val){
-                
+        int max = -1;
+        // Push the max to stack
+        while(reverse != null){
+            if(reverse.val >= max){
+                max = reverse.val;
+                stack.push(reverse.val);
             }
-            else{
-                max = reversed.val;
-                stack.push(reversed.val);
-            }
-            prev = reversed;
-            reversed = reversed.next;
+            reverse = reverse.next;
         }
         
+        // Create new list
         head = new ListNode(stack.pop());
-        prev = head;
+        ListNode prev = head;
         while(!stack.isEmpty()){
             prev.next = new ListNode(stack.pop());
             prev = prev.next;
         }
+        
         return head;
     }
     
     public ListNode reverseList(ListNode head){
-        ListNode prev = null;
         ListNode curr = head;
+        ListNode prev = null;
+        
         while(curr != null){
             ListNode next = curr.next;
             curr.next = prev;
             
-            //update pointers
+            //update the pointers
             prev = curr;
             curr = next;
+            
+            
         }
         
         return prev;
     }
-    
-//     public ListNode deleteNode(ListNode head, int val){
-//         ListNode curr = head;
-//         // delete at start
-//         if(head.val == val){
-//             head = head.next;
-//             return head;
-//         }
-//         //at the end or in middle
-//         ListNode prev = head;
-//         while(curr.next != null && curr.next.val != val ){
-//             prev = curr;
-//             curr = curr.next;
-            
-//         }
-//         prev.next = curr.next;
-//         return head;
-//     }
     
 }
