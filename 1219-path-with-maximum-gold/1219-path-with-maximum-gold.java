dda1a2fe0f8a9bc93009c516ccda74eec7fef120
Pratik Londhe <84666310+pratik-londhe4@users.noreply.github.com>
2024-05-15 01:33:58 +0530

    Time: 120 ms (12.87%), Space: 44.7 MB (10.03%) - LeetHub

--- 1219-path-with-maximum-gold/1219-path-with-maximum-gold.java
+++ 1219-path-with-maximum-gold/1219-path-with-maximum-gold.java
@@ -1,48 +1,58 @@
 class Solution {
     public int getMaximumGold(int[][] grid) {
-        boolean[][] visited = new boolean[grid.length][grid[0].length];
         
         int ans = 0;
-        for(int i = 0 ; i < grid.length ; i++){
-            for(int j= 0 ; j < grid[0].length ; j++){
+        boolean[][] visited = new boolean[grid.length][grid[0].length];
+        
+        for(int i = 0; i < grid.length ; i++){
+            for(int j = 0 ; j < grid[0].length ; j++){
                 if(grid[i][j] != 0){
-                    ans = Math.max(ans,goldMax(grid,visited,i,j) );
+                    ans = Math.max(ans, getGold(grid,visited,i,j));
                 }
             }
         }
         
         return ans;
+        
+        
     }
     
-    public int goldMax(int[][]grid, boolean[][] visited, int row, int col){
-        if(row < 0 || row > grid.length-1 || col < 0 || col > grid[0].length-1){
+    public int getGold(int[][] grid, boolean[][] visited, int row, int col){
+        // check bounds
+        if( row < 0 || row > grid.length - 1 || col < 0 || col > grid[0].length - 1){
             return 0;
         }
-        
+        // if visited
         if(visited[row][col]){
             return 0;
         }
         
+        // if 0 gold
         if(grid[row][col] == 0){
             return 0;
         }
         
+        
         int gold = grid[row][col];
+        
         visited[row][col] = true;
         
-        int top = goldMax(grid, visited, row-1, col);
-        int right = goldMax(grid, visited, row, col+1);
-        int bottom = goldMax(grid, visited, row+1, col);
-        int left = goldMax(grid, visited, row, col-1);
+        int top = getGold(grid,visited,row-1,col);
+        int left = getGold(grid,visited,row,col-1);
+        int bottom = getGold(grid,visited,row+1,col);
+        int right = getGold(grid,visited,row,col+1);
         
         
-        int val[] = new int[]{top,right,bottom,left};
-        Arrays.sort(val);
+        int [] results = new int[]{top,left,bottom,right};
+        Arrays.sort(results);
         
-        gold+=val[val.length-1];
+        gold+=results[results.length -1];
         
         visited[row][col] = false;
+        
         return gold;
         
+        
     }
+    
 }
